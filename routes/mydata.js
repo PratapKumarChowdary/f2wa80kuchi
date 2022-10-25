9aa8e342e11cab3e4b5b959202b4644dada0b2a6
PratapKumarChowdary <S555080@nwmissouri.edu>
2022-10-25 13:02:05 -0500

    Updated Users.js

--- routes/mydata.js
+++ routes/mydata.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  res.render('mydata',{fact: 'Yudu Eswar Vinay Pratap Kumar Kuchi'})
+  res.render('mydata',{name: 'Yudu Eswar Vinay Pratap Kumar Kuchi'})
 });
 
 module.exports = router;
