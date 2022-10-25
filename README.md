f658492cf89e047cc742b4dd09f5def51096ab1d
PratapKumarChowdary <S555080@nwmissouri.edu>
2022-10-25 12:17:59 -0500

    new endpoint

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # f2wa80kuchi
 
 #### Updated remote repo with all the contents of local repo
+
+
+#### Link to Render: []('https://f2wa80kuchi.onrender.com/')
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+ // res.render('mydata',{fact: 'I\'m very much interested in developing web applications, I used to develop wordpress sites, android applications whenever i get free time. I have published an android application on playstore called Mama Ringtones for which i got 1Lakh+ Installs.'})
 });
 
 module.exports = router;
